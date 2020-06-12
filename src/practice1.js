33c9f334c9a305f66cef295a3f42c205af163c83
teng.li2 <teng.li2@thoughworks.com>
2020-06-12 13:37:05 +0800

    [teng/li][2020.6.12] es6 homework

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,17 @@
-const parseData
+const parseData=function(datas){
+
+    const { data } = datas
+    let propName=datas.column
+    let result=[]
+
+    for (let temp of data){
+       let tempResult={}
+       for (let tempProp of propName){
+           tempResult[tempProp.name]=temp.shift()
+       }
+       result.push(tempResult);
+    }
+    return result
+}
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,18 @@
-const inject
+const inject = function (data,instruction) {
+    let result=data;
+    let initial=data.length;
+
+    for (let temp of instruction){
+        let content=temp.content
+        let index=temp.index
+        if(index>=initial){
+            result.splice(index+1,0,content)
+        }else{
+            result.splice(index,0,content)
+        }
+        initial=index;
+    }
+
+    return result
+}
 export { inject };
